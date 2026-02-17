dad5fb06280b23a8502e79851480252efb5f1585
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-17 11:57:07 +0530

    modified my file and used variable

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("hello world")
+"""
+..variable and operators in python Ravi earns ₹25,000 per month.
+He works for 12 months
+
+👉 Calculate his yearly salary.
+"""
+
+ravi_earns=int(input("enter your salary "))
+works_for=12
+yearly_salary=ravi_earns*works_for
+
+print("the annual salary of ravi is ",yearly_salary)
