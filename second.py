2a51522dbbd80f799f8bf821ba558e40b61430fb
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-17 12:16:27 +0530

    new file with conditional statements and problems

--- second.py
+++ second.py
@@ -8,3 +8,20 @@ if(a>=45):
     print("parent")
 else:
     print("dont know")    
+
+    #problems on conditional statements
+    #first one is even or odd we take input from the user and check user input is even or odd number
+
+    inp=int(input("enter a number to check whether a number is even or odd"))
+    if inp%2==0:
+        print("even number")
+    else:
+        print("odd")    
+
+        #Voting Eligibility
+
+age=int(input("enter your to check whether you are eligible to vote or not"))
+if age>=18 and age<=100:
+            print("eligible to vote")
+else:
+            print("appi ning inna age agilla")    
