1712c42cd70da295e857a29c732866960ecde9fb
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-17 12:01:04 +0530

    practised all arthematic operator with one real world problem

--- first.py
+++ first.py
@@ -10,3 +10,21 @@ works_for=12
 yearly_salary=ravi_earns*works_for
 
 print("the annual salary of ravi is ",yearly_salary)
+
+
+a=12
+b=43
+
+print(a*b) #516
+print(a**b) #25397652694505813866070015990659936347412758528
+print(a/b) #0.27906976744186046
+print(a//b) #0
+print(a%b) #12
+print(a+b) #55
+print(a-b) #-31
+
+
+
+
+
+
