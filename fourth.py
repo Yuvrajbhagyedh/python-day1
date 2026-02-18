f20d4a235e9a50c3839c5fc8234f51528469d027
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-18 15:19:45 +0530

    learned about strings inbuitfunction

--- fourth.py
+++ fourth.py
@@ -3,4 +3,8 @@ b=a.title()#this helps to convert all the first letter in a word to uppercase
 c=a.capitalize() #this helps in coverting the first charater to uppercase
 d=a.swapcase() #this is used to swap the cases if the line is in uppercase all the characters are converted in lowercase
 e=a.casefold() #this forcefully turns the characters into lowercase
-print(e)
+
+#alignment and formatting
+
+f=a.zfill(100)
+print(f)
