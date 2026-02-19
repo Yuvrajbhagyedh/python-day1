2a52fcfd9cf6beeaae2009e1fd586c51ffa4a833
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-19 19:15:32 +0530

    dictionaries

--- five.py
+++ five.py
@@ -1,7 +1,11 @@
 #dictionaries in python
 dict={
     "name":"Yuvraj K",
-    "roll no":162
+    "roll no":162,
+    "marks":{
+        "cn":89,
+        "os":56
+    }
 }
 
-print(dict)
+print(dict["marks"])
