6109c089764b7084ea03605d07eaaf9fb7973775
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-19 19:43:35 +0530

    problems

--- probofday.py
+++ probofday.py
@@ -1,6 +1,8 @@
 #the first problem is to print form 1 to 20
 n=int(input("enter the range to check even "))
-for i in range(2,n,2):
-    print(i)
+total=0
+for i in range(2,n+1):
+   if(i%2==0):
+      total+=1
 
-   
+print(total)   
