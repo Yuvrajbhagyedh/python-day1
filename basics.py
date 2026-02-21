0aa7ae9c6936e0e9878208d133a5169063f14261
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-21 15:45:33 +0530

    fizzbuzz problem

---
+++ basics.py
@@ -0,0 +1,16 @@
+
+
+def fizzBuzz(number):
+    
+   if number % 3==0 and number % 5==0:
+       print("FizzBuzz")
+   elif number % 5==0:
+        print("Buzz")
+   elif number % 3==0:
+       print("Fizz")
+       
+   else:
+       print(number)
+        
+    
+    
