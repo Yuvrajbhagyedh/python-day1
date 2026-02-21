cc9bc2d76456a6446efa59a4b283a8b5438c3423
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-21 15:50:31 +0530

    again if elif practise

---
+++ basic.py
@@ -0,0 +1,6 @@
+def friends_in_trouble(j_angry, s_angry):
+    
+    if j_angry==True and s_angry==True or j_angry==False and s_angry==False:
+        return(True)
+    elif j_angry==False and s_angry==True or j_angry==True and s_angry==False:
+        return(False)
