62b877f4acceb2f33fdcfd59962d76c48cc89d10
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-23 20:41:05 +0530

    lerarning two pointers

--- twopointers.py
+++ twopointers.py
@@ -1,4 +1,19 @@
-s="madam"
+s="mada"
 left=0
-right=len(s)
-print(right)
+right=len(s)-1
+
+ispalindrom=True
+while left<right:
+    if s[left]!=s[right]:
+        ispalindrom=False
+        break
+
+    left+=1
+    right-=1
+
+
+if ispalindrom:
+    print("the word is a palindrome")
+
+else:
+    print("not a palindrome")    
