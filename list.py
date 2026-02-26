5af171ba4f3daa7a0d81791190579d3935bd9974
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-26 18:23:16 +0530

    lists

--- list.py
+++ list.py
@@ -1,4 +1,8 @@
-lis=["mathe","13nvs","vsfv","svfv","vsfv"]
-
-lis.append("naan shaa")
-print(lis)
+lis=[1,2,3,1,5,6]
+sit={"yuvraj","Yuvraj",7}
+sit2={1,2,3,4,5,6,7}
+a=sit.intersection(sit2)
+sit2.intersection_update(sit)
+c=lis.index(1,1,6)
+print(a)
+print(sit2)
