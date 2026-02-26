5c426e20fbaf8d261077a633828ce7f665773a50
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-02-26 17:49:18 +0530

    lists

---
+++ list.py
@@ -0,0 +1,4 @@
+lis=["mathe","13nvs","vsfv","svfv","vsfv"]
+
+lis.append("naan shaa")
+print(lis)
--- string.py
+++ string.py
@@ -2,3 +2,5 @@ str="hello1213uvraj"
 c=str.isalnum()
 d=str.__init_subclass__()
 print(c)
+lis=[1,3,42,1,43,23]
+print(lis)
