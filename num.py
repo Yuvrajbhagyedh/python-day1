179e8cdb021cd9af1a14abf90683f16cff12b146
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-04 22:34:58 +0530

    numpy

--- num.py
+++ num.py
@@ -1,5 +1,10 @@
-import numpy as nl
+import numpy as np
 
 lis=[1,2,3,4,5]
-arr=nl.array(lis)
-print(arr)
+lis2=[2,4,5,56,3]
+lis3=[2,4,5,56,3]
+arr=np.array([lis,lis2,lis3])
+
+arr1=arr.reshape(5,3)
+print(arr1)
+print(type(arr))
